d4a47c165160b3189cad46da819c2e3c05e7bf3b
Kevin Sandom <kevinsandom@yahoo.co.uk>
2015-04-20 19:22:21 +0100

    experimentation: Created a prototype UDP server.

---
+++ experimentation/udpServer.php
@@ -0,0 +1,30 @@
+#!/usr/bin/php
+<?php
+# Creates a UDP server on port 1040 and prints the output
+
+$socket = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
+$address = 0;
+socket_bind($socket, $address, 1040);
+
+$buffer = '';
+$length = 1024;
+
+while (true)
+{
+	if ($gotBytes = socket_recv ( $socket , $buffer , $length , MSG_DONTWAIT ))
+	{
+		print "\nGot $gotBytes bytes: $buffer";
+	}
+	else
+	{
+		print ".";
+		usleep (100000);
+	}
+}
+print "Exited loop.";
+
+
+socket_shutdown($socket);
+socket_close($socket);
+
+?>
